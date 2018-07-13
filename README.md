02c72f65564c7bdd20b48c5081a2f93885a2e66a
Idorenyin Obong <idee4ril@gmail.com>
2018-07-13 16:23:17 +0100

    simple app

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hello World App
+
+> A simple helloworld app with Node.js and Express.js
+
+# Setup
+
+- Create a new [Now](now.sh) account and obtain a `NOW_TOKEN`.
+- Encrypt your `NOW_TOKEN` key with [Travis CLI](https://docs.travis-ci.com/user/environment-variables/#Encrypting-environment-variables)
+- Install your dependencies:
+```bash
+npm install
+```
+
+- Start the server:
+```bash
+npm run server
+```
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var path = require("path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
